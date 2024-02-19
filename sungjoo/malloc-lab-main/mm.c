ea5f2551599ac7d61ac4c0629046282bc3dcbd58
SUNGJOOLEE <bbuchu123@gmail.com>
2024-02-19 15:42:54 +0900

    mm_malloc 구현

--- sungjoo/malloc-lab-main/mm.c
+++ sungjoo/malloc-lab-main/mm.c
@@ -154,6 +154,30 @@ static void* coalesce(void* bp){
 
 }
 
+void* mm_malloc(size_t size){
+    size_t asize;
+    size_t extendsize;
+    char* bp;
+    
+    if(size == 0)
+        return NULL;
+    
+    if(size <= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize =DSIZE *((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+    
+    if((bp = find_fit(asize)) != NULL){
+        place(bp,asize);
+        return bp;
+    }
+
+    extendsize =MAX(asize,CHUNKSIZE);
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL;
+    place(bp,asize);
+    return bp;
+}
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
